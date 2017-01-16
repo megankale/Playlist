dd821638083070bbd07cce5c70518ead2ae21f74
Megan Kale <meganleannekale@gmail.com>
2017-01-15 22:50:03 -0500

    Fourth Commit

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
-document.querySelector("#add").addEventListener("submit", function(e){
-  e.preventDefault();
-  var song = document.querySelector("#song").value
-  myPlaylist.addSong(song);
-})
-
 function playlist(){
   this.songs = [];
 
   this.addSong = function(song){
     this.songs.push(song);
     $("#list").append("<li>" + song + "</li>");
+    $("#track-title").html(query);
   }
 
   this.play = function(){
@@ -19,8 +14,65 @@ function playlist(){
   }
 }
 
-var myPlaylist = new playlist()
+function Jukebox(){
+  this.play = function(){
+		playlist.play();
+	}
+  }
 
+var myPlaylist = new playlist()
+var myJukebox = new Jukebox();
+var query= document.querySelector("input").value;
+
+document.querySelector("form").addEventListener("submit", function(e) {
+    e.preventDefault();
+    var audio = document.getElementById('playSong');
+    var query= document.querySelector("input").value;
+    console.log(query);
+    function searchTracks(query) {
+        $.ajax({
+            url: 'https://api.spotify.com/v1/search',
+            data: {
+                q: query,
+                type: 'track'
+            },
+            success: function(response) {
+              var track = response.tracks.items[0]
+              audio.src = track.preview_url
+              var song = audio.src
+              console.log(song);
+              }
+          })
+    }
+    searchTracks(query);
+  })
+
+// function searchTracks(query) {
+//       $.ajax({
+//           url: 'https://api.spotify.com/v1/search',
+//           data: {
+//               q: query,
+//               type: 'track'
+//           },
+//           success: function(response) {
+//             var track = response.tracks.items[0]
+//             audio.src = track.preview_url
+//             var song = audio.src
+//             console.log(song);
+//           }
+//         })
+//       }
+//
+//   document.querySelector("form").addEventListener("submit", function(e) {
+//       e.preventDefault();
+//       searchTracks(query);
+//     })
+
+document.querySelector("#add").addEventListener("submit", function(e){
+  e.preventDefault();
+  var song = document.querySelector("#song").value
+  myPlaylist.addSong(song);
+})
 
 document.getElementById('Play').addEventListener("click", function(e){
   e.preventDefault
