a17e1879b9b2a5b878a49ad0672befba8d1a9502
Megan Kale <meganleannekale@gmail.com>
2017-01-16 19:58:32 -0500

    Seventh- Play Button works,playlist functions, but array does not iterate when played

--- script.js
+++ script.js
@@ -3,14 +3,18 @@ function playlist(){
 
   this.addSong = function(song){
     this.songs.push(song);
-    $("#list").append("<li>" + song + "</li>");
-    $("#track-title").html(query);
+
   }
 
   this.play = function(){
     for (var i = 0; i < this.songs.length; i++){
       console.log(this.songs[i]);
+      audio.src = this.songs[i];
+      audio.play();
     }
+  }
+  this.pause = function(){
+    audio.pause();
   }
 }
 
@@ -23,63 +27,56 @@ function Jukebox(){
 var myPlaylist = new playlist()
 var myJukebox = new Jukebox();
 var query= document.querySelector("input").value;
+var audio = document.getElementById('playSong');
 
 document.querySelector("form").addEventListener("submit", function(e) {
     e.preventDefault();
-    var audio = document.getElementById('playSong');
     var query= document.querySelector("input").value;
     console.log(query);
-    function searchTracks(query) {
-        $.ajax({
-            url: 'https://api.spotify.com/v1/search',
-            data: {
-                q: query,
-                type: 'track'
-            },
-            success: function(response) {
-              var track = response.tracks.items[0]
-              audio.src = track.preview_url
-              var song = audio.src
-              console.log(song);
-              }
-          })
-    }
-    searchTracks(query);
+    searchTracks(query)
+    ;
   })
 
-// function searchTracks(query) {
-//       $.ajax({
-//           url: 'https://api.spotify.com/v1/search',
-//           data: {
-//               q: query,
-//               type: 'track'
-//           },
-//           success: function(response) {
-//             var track = response.tracks.items[0]
-//             audio.src = track.preview_url
-//             var song = audio.src
-//             console.log(song);
-//           }
-//         })
-//       }
-//
-//   document.querySelector("form").addEventListener("submit", function(e) {
-//       e.preventDefault();
-//       searchTracks(query);
-//     })
+function searchTracks(query) {
+    $.ajax({
+        url: 'https://api.spotify.com/v1/search',
+        data: {
+            q: query,
+            type: 'track'
+        },
+        success: function(response) {
+            var track = response.tracks.items[0]
+            // var audio = document.getElementById('playSong');
+            console.log(track.preview_url);
+            audio.src = track.preview_url;
+            $("#list").append("<li>" + query + "</li>");
+            addTracks(audio.src);
+        }
+    })
+}
 
-document.querySelector("#add").addEventListener("submit", function(e){
-  e.preventDefault();
-  var song = document.querySelector("#song").value
+function addTracks(song) {
   myPlaylist.addSong(song);
+}
+
+document.getElementById('Back').addEventListener("click", function(e){
+  e.preventDefault();
+  console.log("back button pressed");
+  myPlaylist.play();
 })
-
-document.getElementsByClassName('fa').addEventListener("click", function(e){
-  e.preventDefault
-  console.log(hello);
-  // myPlaylist.play();
+document.getElementById('Pause').addEventListener("click", function(e){
+  e.preventDefault();
+  console.log("pause button pressed");
+  myPlaylist.pause();
 })
-
-
-
+document.getElementById('Play').addEventListener("click", function(e){
+  e.preventDefault();
+  console.log("play button pressed");
+  myPlaylist.play();
+})
+document.getElementById('Forward').addEventListener("click", function(e){
+  e.preventDefault();
+  console.log("forward button pressed");
+  myPlaylist.play();
+})
 console.log(myPlaylist);
